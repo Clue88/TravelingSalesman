4f69d95123dcaaa82c43fa99c93e04e41e38ca8c
Christopher Liu <christopherliu@sorcero.com>
2021-01-16 00:00:53 -0500

    Add permutation stuff to TravelingSalesman

--- TravelingSalesman.java
+++ TravelingSalesman.java
@@ -13,9 +13,40 @@ public class TravelingSalesman {
             grid[places.indexOf(words[2])][places.indexOf(words[0])] = Integer.parseInt(words[4]);
         }
 
+        // for (int i = 0; i < grid.length; i++) {
+        //     System.out.println(Arrays.toString(grid[i]));
+        // }
+
+        int shortest = Integer.MAX_VALUE;
+        int sequence = 0;
         for (int i = 0; i < grid.length; i++) {
-            System.out.println(Arrays.toString(grid[i]));
+            for (int j = 0; j < grid.length; j++) {
+                if (j == i) continue;
+                for (int k = 0; k < grid.length; k++) {
+                    if (k == i || k == j) continue;
+                    for (int l = 0; l < grid.length; l++) {
+                        if (l == i || l == j || l == k) continue;
+                        for (int m = 0; m < grid.length; m++) {
+                            if (m == i || m == j || m == k || m == l) continue;
+                            for (int n = 0; n < grid.length; n++) {
+                                if (n == i || n == j || n == k || n == l || n == m) continue;
+                                for (int o = 0; o < grid.length; o++) {
+                                    if (o == i || o == j || o == k || o == l || o == m || o == n) continue;
+                                    for (int p = 0; p < grid.length; p++) {
+                                        if (p == i || p == j || p == k || p == l || p == m || p == n || p == o) continue;
+                                        sequence = grid[i][j] + grid[j][k] + grid[k][l] + grid[l][m] + grid[m][n] + grid[n][o] + grid[o][p];
+                                        if (sequence < shortest) shortest = sequence;
+                                        sequence = 0;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
+
+        System.out.println(shortest);
 
         s.close();
     }
